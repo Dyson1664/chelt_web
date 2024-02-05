c927bfee8795d2df1e662046c33a770d38f50837
david reilly <davidreilly02@gmail.com>
2024-02-05 10:44:31 +0700

    1

--- demo2/demo2/settings.py
+++ demo2/demo2/settings.py
@@ -150,9 +150,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+if not DEBUG:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new
 # Following settings only make sense on production and may break development environments.
 # if not DEBUG:
 #     # Tell Django to copy statics to the `staticfiles` directory
